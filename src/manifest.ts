230f2d5690b1327e5859705418bf28c68c23ffff
Vlad Tansky <vl.tansky@gmail.com>
2023-03-20 11:26:45 +0200

    x

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   name: 'create-chrome-ext',
   description: '',
-  version: '0.0.0',
+  version: '0.0.1',
   manifest_version: 3,
   icons: {
     '16': 'img/logo-16.png',
